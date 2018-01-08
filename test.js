9e61489e94f026ba3c75d496d0663e8acea68799
A88245 <lindam.zhang@arrowasia.com>
2018-01-08 16:28:04 +0800

    提供提交application/json

--- test.js
+++ test.js
@@ -17,26 +17,26 @@ Model.listdata({
      console.log(result.errmsg);
 });
 
-// Model.listdata({
-//      data: {
-//          username: 'zmr',
-//          sex: '女',
-//          arrdata: [{
-//            age: 10,
-//            interest: 'aaa'
-//          }]
-//      },
-//      headers: {
-//        'Content-Type': 'application/json'
-//      },
-//      //获取response对象
-//      getResponse: function(response){
-//          console.log(response);
-//      }
-//      //不接受统一的业务错误处理IoConfig.ioparams.fail
-//      //  fail: null
-// }).then(function(list){ //业务返回正确
-//      console.log(list);
-// }).catch(function(result){ //业务错误
-//      console.log(result.errmsg);
-// });
+Model.listdata({
+     data: JSON.stringify({
+         username: 'zmr',
+         sex: '女',
+         arrdata: [{
+           key1: 'aaa',
+           key2: 'bbb'
+         }]
+     }),
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     //获取response对象
+     getResponse: function(response){
+         console.log(response);
+     }
+     //不接受统一的业务错误处理IoConfig.ioparams.fail
+     //  fail: null
+}).then(function(list){ //业务返回正确
+     console.log(list);
+}).catch(function(result){ //业务错误
+     console.log(result.errmsg);
+});
